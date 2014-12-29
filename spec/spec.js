76cc6fffd668900fdc6ed4fc9696a48a4ffb34da
Zhaoyu Wei <weixiaobo88@gmail.com>
2014-12-29 15:40:36 +0800

    zy: init project

---
+++ spec/spec.js
@@ -0,0 +1,10 @@
+describe('Pos ', function(){
+    describe('input format', function(){
+
+        it('should generate input as json', function(){
+            expect(1).toBe(1);
+        });
+    });
+});
+
+
